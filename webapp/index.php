9095c2e87001e7a5406ed8a355033371bf6ca12f
Stephan Hug <stephan.hug@adfinis.com>
2022-12-19 13:59:39 +0100

    add table

--- webapp/index.php
+++ webapp/index.php
@@ -1,14 +1,63 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
 <h1>Welcome to Productive!</h1>
-<?php require "view/blocks/page_end.php"; ?>
+
+<h2>Our most popular products</h2>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Stock</th>
+    </tr>
+  </thead>
+  <tbody id="products-table">
+  </tbody>
+</table>
 
 <script>
-class UserService {
-  validateUsername = function (username) {
-    // implement validation here
-  }
+const updateProductTable = async function (table, productsToInsert) {
+  const columns = ['name', 'price', 'stock'];
+
+  // Insert the table rows for the found products
+  const newTableRows = productsToInsert.map((product) => {
+    let thing = document.createElement('tr');
+
+    const tds = [];
+
+    for (let i = 0; i < columns.length; i++) {
+      let column = columns[i];
+
+      let td = document.createElement('td');
+      if(column == 'stock') {
+        if (product[column] <= 3) {
+          td.style.color = 'red'
+        } else {
+          td.style.color = 'black';
+        }
+      }
+      td.innerText = product[column]; // dunno lol
+      thing.appendChild(td);
+    }
+
+    return thing
+  })
+
+  table.replaceChildren(...newTableRows)
 }
 
-window.UserService = UserService;
+const init = async function () { // an async wrapper function which allows me to use await instead of .then()
+  const result1 = await fetch('API/V1/Products')
+  const _products = await result1.json()
+  const products = _products.filter((product) => product.active === "1")
+
+  console.log(products)
+
+  updateProductTable(document.querySelector('#products-table'), products);
+}
+
+init();
 </script>
+
+<?php require "view/blocks/page_end.php"; ?>
